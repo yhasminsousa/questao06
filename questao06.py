0d8d4bfaa1f9b2b4d1c30579df9ccacf4ba29544
palomalindinha01 <palomalindinha35@gmail.com>
2023-10-17 16:32:04 -0300

    first commit

---
+++ questao06.py
@@ -0,0 +1,23 @@
+# Digite um numero interio e veja sua tabuada
+
+numero = float(input("Digite um numero \n"))
+
+ta1 = numero * 1
+ta2 = numero * 2
+ta3 = numero * 3
+ta4 = numero * 4
+ta5 = numero * 5
+ta6 = numero * 6
+ta7 = numero * 7
+ta8 = numero * 8
+ta9 = numero * 9
+
+print(ta1)
+print(ta2)
+print(ta3)
+print(ta4)
+print(ta5)
+print(ta6)
+print(ta7)
+print(ta8)
+print(ta9)
